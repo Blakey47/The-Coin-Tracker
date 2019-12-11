13283bfb2b07daff99f4db0f9ac14652f34233e3
Darragh <darragh.blake@urbanairship.com>
2019-12-11 18:47:21 +0000

    Initial Commit

---
+++ The Coin Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Coin Tracker
+//
+//  Created by Darragh Blake on 11/12/2019.
+//  Copyright © 2019 Darragh Blake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
