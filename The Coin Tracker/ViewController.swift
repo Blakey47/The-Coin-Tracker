0a5b11ef3d553729e5d04431dbc29a71b730eb12
Darragh <darragh.blake@urbanairship.com>
2019-12-12 20:01:25 +0000

    Animated Menu

--- The Coin Tracker/ViewController.swift
+++ The Coin Tracker/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var menuView: UIView!
+    @IBOutlet weak var menuButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupUI()
+    }
+    
+    func setupUI() {
+        setupPickerView()
+    }
+    
+    func setupPickerView() {
+        menuView.layer.cornerRadius = 30
     }
 
+    @IBAction func toggleMenuButton(_ sender: Any) {
+        
+        if menuView.transform == .identity {
+            UIView.animate(withDuration: 1, animations: {
+                self.menuView.backgroundColor = .white
+                self.menuView.transform = CGAffineTransform(translationX: 0, y: -283)
+                self.menuButton.setBackgroundImage(UIImage(systemName: "minus.circle"), for: UIControl.State.normal)
+                self.menuButton.transform = CGAffineTransform(translationX: 0, y: -290)
+            }) { (true) in
+                
+            }
+        } else {
+            UIView.animate(withDuration: 1, animations: {
+                self.menuView.transform = .identity
+                self.menuView.backgroundColor = .clear
+                self.menuButton.transform = .identity
+                self.menuButton.setBackgroundImage(UIImage(systemName: "plus.circle"), for: UIControl.State.normal)
+            }) { (true) in
+                
+            }
+        }
+        
+    }
+    
 
 }
+
 
